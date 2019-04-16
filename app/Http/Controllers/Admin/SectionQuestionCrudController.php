0173e039284e310b292a74c13d2d90ddba165ec8
Stanko <skruzic@gmail.com>
2019-04-16 18:14:01 +0200

    Dodan WYSIWYG editor za pitanja

--- app/Http/Controllers/Admin/SectionQuestionCrudController.php
+++ app/Http/Controllers/Admin/SectionQuestionCrudController.php
@@ -16,205 +16,202 @@ use App\Models\Section;
  * @package App\Http\Controllers\Admin
  * @property-read CrudPanel $crud
  */
-class SectionQuestionCrudController extends CrudController
-{
-    public function setup()
-    {
-        //parent::setup();
-        /*
-        |--------------------------------------------------------------------------
-        | CrudPanel Basic Information
-        |--------------------------------------------------------------------------
-        */
-        $campaign_id = \Route::current()->parameter('campaign_id');
-        $section_id  = \Route::current()->parameter('section_id');
-        $this->crud->setModel('App\Models\Question');
-        $this->crud->setRoute('admin/campaign/'.$campaign_id.'/section/'.$section_id.'/question');
-        $this->crud->setEntityNameStrings(__('admin.question'), __('admin.questions'));
-        $this->crud->addClause('where', 'section_id', $section_id);
-        $this->crud->orderBy('lft');
-        $this->crud->setHeading(__('admin.questions_in_section',
-            ['section' => $section_id, 'campaign' => $campaign_id]), 'index');
+class SectionQuestionCrudController extends CrudController {
+	public function setup() {
+		//parent::setup();
+		/*
+		|--------------------------------------------------------------------------
+		| CrudPanel Basic Information
+		|--------------------------------------------------------------------------
+		*/
+		$campaign_id = \Route::current()->parameter( 'campaign_id' );
+		$section_id  = \Route::current()->parameter( 'section_id' );
+		$this->crud->setModel( 'App\Models\Question' );
+		$this->crud->setRoute( 'admin/campaign/' . $campaign_id . '/section/' . $section_id . '/question' );
+		$this->crud->setEntityNameStrings( __( 'admin.question' ), __( 'admin.questions' ) );
+		$this->crud->addClause( 'where', 'section_id', $section_id );
+		$this->crud->orderBy( 'lft' );
+		$this->crud->setHeading( __( 'admin.questions_in_section',
+			[ 'section' => $section_id, 'campaign' => $campaign_id ] ), 'index' );
 
-        // Botun za povratak na sekciju
-        $this->crud->addButtonFromView('top', 'campaign_sections', 'campaign_sections_question', 'end');
+		// Botun za povratak na sekciju
+		$this->crud->addButtonFromView( 'top', 'campaign_sections', 'campaign_sections_question', 'end' );
 
-        /*
-        |--------------------------------------------------------------------------
-        | CrudPanel Configuration
-        |--------------------------------------------------------------------------
-        */
+		/*
+		|--------------------------------------------------------------------------
+		| CrudPanel Configuration
+		|--------------------------------------------------------------------------
+		*/
 
-        // TODO: remove setFromDb() and manually define Fields and Columns
-        //$this->crud->setFromDb();
-        $this->crud->addColumns([
-            [
-                'name'  => 'title',
-                'label' => 'Kod',
-                'type'  => 'text',
-            ],
-            [
-                'name'  => 'question',
-                'label' => ucfirst(__('admin.question')),
-                'type'  => 'text',
-            ],
-            [
-                'name'    => 'type',
-                'label'   => __('admin.type'),
-                'type'    => 'select_from_array',
-                'options' => QuestionType::toArray(),
-            ],
-            [
-                'name'      => 'options',
-                'label'     => __('admin.options'),
-                'type'      => 'select',
-                'entity'    => 'options',
-                'attribute' => 'name',
-                'model'     => 'App\Models\OptionGroup',
-            ],
-        ]);
+		// TODO: remove setFromDb() and manually define Fields and Columns
+		//$this->crud->setFromDb();
+		$this->crud->addColumns( [
+			[
+				'name'  => 'title',
+				'label' => 'Kod',
+				'type'  => 'text',
+			],
+			[
+				'name'  => 'question',
+				'label' => ucfirst( __( 'admin.question' ) ),
+				'type'  => 'text',
+			],
+			[
+				'name'    => 'type',
+				'label'   => __( 'admin.type' ),
+				'type'    => 'select_from_array',
+				'options' => QuestionType::toArray(),
+			],
+			[
+				'name'      => 'options',
+				'label'     => __( 'admin.options' ),
+				'type'      => 'select',
+				'entity'    => 'options',
+				'attribute' => 'name',
+				'model'     => 'App\Models\OptionGroup',
+			],
+		] );
 
-        $this->crud->addFields([
-            [
-                'name'  => 'title',
-                'label' => 'Kod',
-                'type'  => 'text',
-                'tab'   => __('admin.general_tab'),
-            ],
-            [
-                'name'  => 'question',
-                'label' => ucfirst(__('admin.question')),
-                'type'  => 'textarea',
-                'tab'   => __('admin.general_tab'),
-            ],
-            [
-                'name'    => 'type',
-                'label'   => __('admin.type'),
-                'type'    => 'select2_from_array',
-                'options' => QuestionType::toArray(),
-                'tab'     => __('admin.general_tab'),
-            ],
-            [
-                'name'      => 'option_group_id',
-                'label'     => __('admin.options'),
-                'type'      => 'select2',
-                'entity'    => 'options',
-                'attribute' => 'name',
-                'model'     => 'App\Models\OptionGroup',
-                'tab'       => __('admin.general_tab'),
-            ],
-            /*[
-                'name'                       => 'section_id',
-                'label'                      => __('admin.section'),
-                'type'                       => 'select2_grouped',
-                //'value' => $section_id,
-                'entity'                     => 'section',
-                'attribute'                  => 'title',
-                //'model' => 'App\Models\Section',
-                'group_by'                   => 'campaign',
-                'group_by_attribute'         => 'title',
-                'group_by_relationship_back' => 'sections',
-                'tab'                        => __('admin.general_tab'),
-            ],*/
-            /*[
-                'name'   => 'csv',
-                'label'  => 'Panel',
-                'type'   => 'upload',
-                'upload' => true,
-                'disk'   => 'uploads',
-                'tab'    => __('admin.general_tab'),
-            ],*/
-            [
-                'name'                       => 'section_id',
-                'label'                      => 'Section',
-                'type'                       => 'select2_grouped',
-                //'value' => $section_id,
-                'entity'                     => 'section',
-                'attribute'                  => 'title',
-                //'model' => 'App\Models\Section',
-                'group_by'                   => 'campaign',
-                'group_by_attribute'         => 'title',
-                'group_by_relationship_back' => 'sections',
-                'value'                      => $section_id,
-                'tab'                        => __('admin.general_tab'),
-            ],
-            [
-                'name'   => 'csv',
-                'label'  => 'Panel',
-                'type'   => 'upload',
-                'upload' => true,
-                'disk'   => 'uploads',
-                'tab'    => __('admin.panel_tab'),
-            ],
-            [
-                'name'  => 'dynamic',
-                'label' => 'Dynamic',
-                'type'  => 'checkbox',
-                'fake'  => true,
-                'tab'    => __('admin.panel_tab'),
-            ],
-            [
-                'name'  => 'timeout',
-                'label' => 'Timeout (s)',
-                'type'  => 'number',
-                'fake'  => true,
-                'tab'    => __('admin.panel_tab'),
-            ],
-            [
-                'name'    => 'batch_size',
-                'label'   => 'Batch size',
-                'type'    => 'number',
-                'default' => 5,
-                'fake'    => true,
-                'tab'    => __('admin.panel_tab'),
-            ],
-        ]);
+		$this->crud->addFields( [
+			[
+				'name'  => 'title',
+				'label' => 'Kod',
+				'type'  => 'text',
+				'tab'   => __( 'admin.general_tab' ),
+			],
+			[
+				'name'  => 'question',
+				'label' => ucfirst( __( 'admin.question' ) ),
+				//'type'  => 'textarea',
+				'type'  => 'summernote',
+				'tab'   => __( 'admin.general_tab' ),
+			],
+			[
+				'name'    => 'type',
+				'label'   => __( 'admin.type' ),
+				'type'    => 'select2_from_array',
+				'options' => QuestionType::toArray(),
+				'tab'     => __( 'admin.general_tab' ),
+			],
+			[
+				'name'      => 'option_group_id',
+				'label'     => __( 'admin.options' ),
+				'type'      => 'select2',
+				'entity'    => 'options',
+				'attribute' => 'name',
+				'model'     => 'App\Models\OptionGroup',
+				'tab'       => __( 'admin.general_tab' ),
+			],
+			/*[
+				'name'                       => 'section_id',
+				'label'                      => __('admin.section'),
+				'type'                       => 'select2_grouped',
+				//'value' => $section_id,
+				'entity'                     => 'section',
+				'attribute'                  => 'title',
+				//'model' => 'App\Models\Section',
+				'group_by'                   => 'campaign',
+				'group_by_attribute'         => 'title',
+				'group_by_relationship_back' => 'sections',
+				'tab'                        => __('admin.general_tab'),
+			],*/
+			/*[
+				'name'   => 'csv',
+				'label'  => 'Panel',
+				'type'   => 'upload',
+				'upload' => true,
+				'disk'   => 'uploads',
+				'tab'    => __('admin.general_tab'),
+			],*/
+			[
+				'name'                       => 'section_id',
+				'label'                      => 'Section',
+				'type'                       => 'select2_grouped',
+				//'value' => $section_id,
+				'entity'                     => 'section',
+				'attribute'                  => 'title',
+				//'model' => 'App\Models\Section',
+				'group_by'                   => 'campaign',
+				'group_by_attribute'         => 'title',
+				'group_by_relationship_back' => 'sections',
+				'value'                      => $section_id,
+				'tab'                        => __( 'admin.general_tab' ),
+			],
+			[
+				'name'   => 'csv',
+				'label'  => 'Panel',
+				'type'   => 'upload',
+				'upload' => true,
+				'disk'   => 'uploads',
+				'tab'    => __( 'admin.panel_tab' ),
+			],
+			[
+				'name'  => 'dynamic',
+				'label' => 'Dynamic',
+				'type'  => 'checkbox',
+				'fake'  => true,
+				'tab'   => __( 'admin.panel_tab' ),
+			],
+			[
+				'name'  => 'timeout',
+				'label' => 'Timeout (s)',
+				'type'  => 'number',
+				'fake'  => true,
+				'tab'   => __( 'admin.panel_tab' ),
+			],
+			[
+				'name'    => 'batch_size',
+				'label'   => 'Batch size',
+				'type'    => 'number',
+				'default' => 5,
+				'fake'    => true,
+				'tab'     => __( 'admin.panel_tab' ),
+			],
+		] );
 
-        $this->crud->allowAccess('create');
-        $this->crud->allowAccess('reorder');
-        $this->crud->enableReorder('question', 2);
+		$this->crud->allowAccess( 'create' );
+		$this->crud->allowAccess( 'reorder' );
+		$this->crud->enableReorder( 'question', 2 );
 
-        // Micem polje za upload CSV-a kod updejta (mjenjanja postavki)
-        $this->crud->removeField('csv', 'update');
+		// Micem polje za upload CSV-a kod updejta (mjenjanja postavki)
+		$this->crud->removeField( 'csv', 'update' );
 
-        // add asterisk for fields that are required in QuestionRequest
-        $this->crud->setRequiredFields(StoreRequest::class, 'create');
-        $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
-    }
+		// add asterisk for fields that are required in QuestionRequest
+		$this->crud->setRequiredFields( StoreRequest::class, 'create' );
+		$this->crud->setRequiredFields( UpdateRequest::class, 'edit' );
+	}
 
-    public function store(StoreRequest $request)
-    {
-        if ($request->hasFile('csv')) {
+	public function store( StoreRequest $request ) {
+		if ( $request->hasFile( 'csv' ) ) {
 
-            $file = $request->file('csv');
+			$file = $request->file( 'csv' );
 
-            // Obrada CSV filea
-            //$csv = PanelImporter::import($file);
-            $csv = (new PanelImporter($file))->import();
+			// Obrada CSV filea
+			//$csv = PanelImporter::import($file);
+			$csv = ( new PanelImporter( $file ) )->import();
 
-            $panel = json_encode($csv);
+			$panel = json_encode( $csv );
 
-            $request->request->add(['panel' => $panel]);
-        }
+			$request->request->add( [ 'panel' => $panel ] );
+		}
 
-        return parent::storeCrud($request);
-    }
+		return parent::storeCrud( $request );
+	}
 
-    public function update(UpdateRequest $request)
-    {
-        /*if ($request->hasFile('csv')) {
+	public function update( UpdateRequest $request ) {
+		/*if ($request->hasFile('csv')) {
 
-            $file = $request->file('csv');
+			$file = $request->file('csv');
 
-            // Obrada CSV filea
-            //$csv = PanelImporter::import($file);
-            $csv = (new PanelImporter($file))->import();
+			// Obrada CSV filea
+			//$csv = PanelImporter::import($file);
+			$csv = (new PanelImporter($file))->import();
 
-            $panel = json_encode($csv);
+			$panel = json_encode($csv);
 
-            $request->request->add(['panel' => $panel]);
-        }*/
+			$request->request->add(['panel' => $panel]);
+		}*/
 
-        return parent::updateCrud($request);
-    }
+		return parent::updateCrud( $request );
+	}
 }
