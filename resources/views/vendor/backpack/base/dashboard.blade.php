db0fb6fdd7506f293974f5cd0f42eecaad9d5a83
Stanko Kruzic <skruzic@gmail.com>
2021-01-23 18:59:06 +0100

    Malo sredjen dashboard s brojkama

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -2,11 +2,42 @@
 
 @php
     $widgets['before_content'][] = [
-        'type'        => 'jumbotron',
-        'heading'     => trans('backpack::base.welcome'),
-        'content'     => trans('backpack::base.use_sidebar'),
-        'button_link' => backpack_url('logout'),
-        'button_text' => trans('backpack::base.logout'),
+        'type'      => 'div',
+        'class'     => 'row',
+        'content'   => [
+            [
+                'type'          => 'progress_white',
+                'class'         => 'card mb-2',
+                'value'         => \App\Models\User::count(),
+                'description'   => 'Users',
+                'progress'      => 100, // integer
+                'progressClass' => 'progress-bar bg-primary',
+            ],
+            [
+                'type'          => 'progress_white',
+                'class'         => 'card mb-2',
+                'value'         => \App\Models\Campaign::count(),
+                'description'   => 'Campaigns',
+                'progress'      => 100, // integer
+                'progressClass' => 'progress-bar bg-info',
+            ],
+            [
+                'type'          => 'progress_white',
+                'class'         => 'card mb-2',
+                'value'         => \App\Models\Question::count(),
+                'description'   => 'Questions',
+                'progress'      => 100, // integer
+                'progressClass' => 'progress-bar bg-warning',
+            ],
+            [
+                'type'        => 'progress_white',
+                'class'       => 'card mb-2',
+                'value'       => \App\Models\Response::count(),
+                'description' => 'Campaign responses',
+                'progress'    => 100, // integer
+                'progressClass' => 'progress-bar bg-dark',
+            ],
+        ]
     ];
 @endphp
 
