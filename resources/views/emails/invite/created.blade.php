e30f06e61a7ba3b2aa3f18a358f7d7a3008ca30f
Stanko Kruzic <skruzic@gmail.com>
2021-04-21 18:25:16 +0200

    Popravljeni linkovi u e-mailu

--- resources/views/emails/invite/created.blade.php
+++ resources/views/emails/invite/created.blade.php
@@ -1,5 +1,5 @@
 <div>
     {!! $invite->campaign->messages['email_before'] !!}
-    {{ url('/'.$invite->campaign->slug.'?token='.$invite->token) }}
+    {{ url('/campaign/'.$invite->campaign->slug.'?token='.$invite->token) }}
     {!! $invite->campaign->messages['email_after'] !!}
 </div>
